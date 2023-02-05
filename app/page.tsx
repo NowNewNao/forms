eecaa1762b5d2b13d978eac5878b51968a866b8c
Nao <45124890+NowNewNao@users.noreply.github.com>
2023-02-05 11:09:04 +0900

    introduce chakraui
    fix: use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+'use client';
 import { Inter } from '@next/font/google';
+import { Card } from '@chakra-ui/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
         <main>
-            <div>hello</div>
+            <Card>Hello</Card>
         </main>
     );
 }
